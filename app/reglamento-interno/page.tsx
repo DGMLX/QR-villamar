2808a5e3d3e96967fa250991a3c4443a2fa83eef
Diego Altamirano <diegoalt.g@hotmail.com>
2025-01-20 16:50:57 -0300

    solucionando error de produccion

--- app/reglamento-interno/page.tsx
+++ app/reglamento-interno/page.tsx
@@ -3,7 +3,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { IoIosArrowRoundForward } from 'react-icons/io'
 import { MdArrowBack } from "react-icons/md";
 
 export default function ReglamentoInterno() {
